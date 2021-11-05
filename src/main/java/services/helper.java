8b4aa4a2cda95f6ecb15ac69f0d38bf592d4f484
PhanQuocPhu <phanquocphu1998@gmail.com>
2021-11-05 22:04:22 +0700

    Test helper

--- src/main/java/services/helper.java
+++ src/main/java/services/helper.java
@@ -1,10 +1,14 @@
 package services;
 
 import com.github.slugify.Slugify;
+import org.apache.commons.lang3.RandomStringUtils;
 
 import java.io.File;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 import java.sql.Timestamp;
 import java.util.Arrays;
+import java.util.Random;
 
 public class helper {
     public static String createSlug(String string) {
@@ -17,5 +21,8 @@ public class helper {
         java.util.Date today = new java.util.Date();
         return new java.sql.Timestamp(today.getTime());
     }
-
+    public static String randString(int length) {
+        String generatedString = RandomStringUtils.randomAlphanumeric(length);
+        return generatedString;
+    }
 }
