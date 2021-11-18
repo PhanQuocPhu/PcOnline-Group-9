194994e4a5d3cea5d63368b0a918f5dc883f54a0
PhanQuocPhu <phanquocphu1998@gmail.com>
2021-11-18 10:38:09 +0700

    Code cho thanh toán vnpay - chưa xong
    - Code thử nghiệm cho thanh toán vnpay -- Chưa test

--- src/main/java/controllers/TransactionController.java
+++ src/main/java/controllers/TransactionController.java
@@ -1,25 +1,30 @@
 package controllers;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
 import entity.Categories;
 import entity.Orders;
 import entity.Transactions;
 import entity.Users;
 import models.OrdersModel;
 import models.TransactionsModel;
-import models.UsersModel;
-import utils.ServletUtils;
+import services.VnpayConst;
 import services.helper;
+import utils.ServletUtils;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 import java.sql.SQLException;
 import java.sql.Timestamp;
-import java.util.List;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 @WebServlet(name = "TransactionController", value = "/home/cart/checkout/*")
 public class TransactionController extends FrontEndController {
@@ -30,7 +35,7 @@ public class TransactionController extends FrontEndController {
         if (path == null || path.equals("/")) {
             path = "/index";
         }
-        switch (path){
+        switch (path) {
             case "/placeorder":
             case "/vnpay":
                 manageTransaction(request, response, path);
@@ -64,9 +69,9 @@ public class TransactionController extends FrontEndController {
         String address = request.getParameter("address");
         String email = request.getParameter("email");
         String phone = request.getParameter("phone");
-        String note =request.getParameter("note");
+        String note = request.getParameter("note");
         Timestamp timestamp = helper.getCurrentTimeStamp();
-        switch (path){
+        switch (path) {
             case "/placeorder":
                 transaction.setId(id);
                 transaction.setUsersByTruserid(user);
@@ -85,12 +90,93 @@ public class TransactionController extends FrontEndController {
                 session.removeAttribute("cart");
                 break;
             case "/vnpay":
+                vnpay(request, response);
                 break;
         }
     }
 
+    private void vnpay(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        Calendar cld = Calendar.getInstance(TimeZone.getTimeZone("Etc/GMT+7"));
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyyMMddHHmmss");
+        //Khai báo const
+        String vnp_Version = "2.1.0";
+        String vnp_Command = "pay";
+        String vnp_TmnCode = VnpayConst.vnp_TmnCode;
+        int amount = Integer.parseInt(request.getParameter("amount")) * 100;
+        String bank_code = request.getParameter("bankcode");
+        String vnp_CreateDate = formatter.format(cld.getTime());
+        String vnp_CurrCode = "VND";
+        String vnp_IpAddr = helper.getIpAddress(request);
+        String locate = request.getParameter("language");
+        String vnp_OrderInfo = "Thanh toán đơn hàng";
+        String orderType = "billpayment";
+        String vnp_TxnRef = helper.randString(8);
 
-    private int getNewTransId(){
+        //Tạo json param
+
+        Map<String, String> vnp_Params = new HashMap<>();
+        vnp_Params.put("vnp_Version", vnp_Version);
+        vnp_Params.put("vnp_Command", vnp_Command);
+        vnp_Params.put("vnp_TmnCode", vnp_TmnCode);
+        vnp_Params.put("vnp_Amount", String.valueOf(amount));
+        vnp_Params.put("vnp_CurrCode", vnp_CurrCode);
+        if (bank_code != null && !bank_code.isEmpty()) {
+            vnp_Params.put("vnp_BankCode", bank_code);
+        }
+        vnp_Params.put("vnp_TxnRef", vnp_TxnRef);
+        vnp_Params.put("vnp_OrderInfo", vnp_OrderInfo);
+        vnp_Params.put("vnp_OrderType", orderType);
+        if (locate != null && !locate.isEmpty()) {
+            vnp_Params.put("vnp_Locale", locate);
+        } else {
+            vnp_Params.put("vnp_Locale", "vn");
+        }
+        vnp_Params.put("vnp_ReturnUrl", VnpayConst.vnp_Returnurl);
+        vnp_Params.put("vnp_IpAddr", vnp_IpAddr);
+        vnp_Params.put("vnp_CreateDate", vnp_CreateDate);
+        cld.add(Calendar.MINUTE, 15);
+        String vnp_ExpireDate = formatter.format(cld.getTime());
+        //Add Params of 2.0.1 Version
+        vnp_Params.put("vnp_ExpireDate", vnp_ExpireDate);
+
+        //Build data to hash and querystring
+        List fieldNames = new ArrayList(vnp_Params.keySet());
+        Collections.sort(fieldNames);
+        StringBuilder hashData = new StringBuilder();
+        StringBuilder query = new StringBuilder();
+        Iterator itr = fieldNames.iterator();
+        while (itr.hasNext()) {
+            String fieldName = (String) itr.next();
+            String fieldValue = (String) vnp_Params.get(fieldName);
+            if ((fieldValue != null) && (fieldValue.length() > 0)) {
+                //Build hash data
+                hashData.append(fieldName);
+                hashData.append('=');
+                hashData.append(URLEncoder.encode(fieldValue, StandardCharsets.US_ASCII.toString()));
+                //Build query
+                query.append(URLEncoder.encode(fieldName, StandardCharsets.US_ASCII.toString()));
+                query.append('=');
+                query.append(URLEncoder.encode(fieldValue, StandardCharsets.US_ASCII.toString()));
+                if (itr.hasNext()) {
+                    query.append('&');
+                    hashData.append('&');
+                }
+            }
+        }
+        String queryUrl = query.toString();
+        String vnp_SecureHash = VnpayConst.hmacSHA512(VnpayConst.vnp_HashSecret, hashData.toString());
+        queryUrl += "&vnp_SecureHash=" + vnp_SecureHash;
+        String paymentUrl = VnpayConst.vnp_PayUrl + "?" + queryUrl;
+        JsonObject job = new JsonObject();
+        job.addProperty("code", "00");
+        job.addProperty("message", "success");
+        job.addProperty("data", paymentUrl);
+        Gson gson = new Gson();
+        response.getWriter().write(gson.toJson(job));
+
+    }
+
+    private int getNewTransId() {
         int id = 0;
         try {
             id = TransactionsModel.getNewId();
--- src/main/java/services/helper.java
+++ src/main/java/services/helper.java
@@ -3,9 +3,7 @@ package services;
 import com.github.slugify.Slugify;
 import org.apache.commons.lang3.RandomStringUtils;
 
-import java.io.File;
-import java.nio.charset.Charset;
-import java.nio.charset.StandardCharsets;
+import javax.servlet.http.HttpServletRequest;
 import java.sql.Timestamp;
 import java.util.Arrays;
 import java.util.Random;
@@ -21,8 +19,33 @@ public class helper {
         java.util.Date today = new java.util.Date();
         return new java.sql.Timestamp(today.getTime());
     }
+
     public static String randString(int length) {
         String generatedString = RandomStringUtils.randomAlphanumeric(length);
         return generatedString;
     }
+
+    public static String randNumber(int length) {
+        Random rnd = new Random();
+        String chars = "0123456789";
+        StringBuilder sb = new StringBuilder(length);
+        for (int i = 0; i < length; i++) {
+            sb.append(chars.charAt(rnd.nextInt(chars.length())));
+        }
+        return sb.toString();
+    }
+
+    public static String getIpAddress(HttpServletRequest request) {
+        String ipAdress;
+        try {
+            ipAdress = request.getHeader("X-FORWARDED-FOR");
+            if (ipAdress == null) {
+                ipAdress = request.getRemoteAddr();
+            }
+        } catch (Exception e) {
+            ipAdress = "Invalid IP:" + e.getMessage();
+        }
+        return ipAdress;
+    }
+
 }
